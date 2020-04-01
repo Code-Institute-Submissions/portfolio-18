7582b22c6bc220897a32841d3ca861b38b7621fc
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 22:23:13 +0100

    changed routing path

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 @app.route("/about")
 def about():
