855234749863960c5eacd6153c78971a246be515
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 20:42:09 +0100

    added routing for register.html

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ def skills():
 def contact():
     return render_template('contact.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
+
 
 
 
