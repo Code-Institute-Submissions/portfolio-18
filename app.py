3b3025ec3cf872e99a712114d21f64f661b7f15b
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 22:39:38 +0100

    changed path to routing

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for
 import os
 from os import path
-if path.exists("env.py"):
-  import venv 
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -18,23 +16,23 @@ def hello():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('pages/about.html')
 
 @app.route("/skills")
 def skills():
-    return render_template('skills.html') 
+    return render_template('pages/skills.html') 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template('pages/contact.html')
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('pages/login.html')
 
 @app.route("/register")
 def register():
-    return render_template('register.html')
+    return render_template('pages/register.html')
 
 
 
@@ -42,6 +40,6 @@ def register():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5000,
-            debug=True)
+    app.run(host=os.environ.get('HOSTNAME'),
+            port=os.environ.get('PORT'),
+            debug=os.environ.get('DEV'))
