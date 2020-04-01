35123ed01ebeb10021f940067fe777d8d71ab765
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 17:08:15 +0100

    changed app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-
-from flask import Flask, render_template, url_for
-import os
-from os import path
-
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
-MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-MONGO_URI = os.environ.get('MONGO_URI')
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('pages/index.html')
-
+@app.route("/")
+def hello():
+    return render_template('index.html')
+    
 if __name__ == '__main__':
-      app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=True)
