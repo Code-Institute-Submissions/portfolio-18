360fbfb406456ba8c0ab33b189d9afa9a9c6c954
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 21:03:20 +0100

    added import os

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, url_for
+import os
+from os import path
+if path.exists("env.py"):
+  import venv 
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+MONGO_URI = os.environ.get('MONGO_URI')
+
+
 
 
 app = Flask(__name__)
