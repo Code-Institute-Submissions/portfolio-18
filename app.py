e0b7fa8ac81cf38df69b932e4ab4d44775b900a7
Patrycja <patrycjatyra@protonmail.com>
2020-04-01 17:05:08 +0100

    removed path exist

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 from flask import Flask, render_template, url_for
 import os
 from os import path
-if path.exists("venv.py"):
- import venv 
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
