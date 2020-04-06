3270c5279f407ec3fa446cac54be3f4f8f70ace2
Patrycja <patrycjatyra@protonmail.com>
2020-04-06 14:16:29 +0100

    changed routing

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['GET', 'POST'])
 def home_page():
-    return render_template('pages/base.html')
+    return render_template('layout/base.html')
 
 @app.route("/about")
 def about():
