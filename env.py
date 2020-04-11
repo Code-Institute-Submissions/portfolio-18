2c1994c42a982b220906140ef67b36d166ff3281
Patrycja <patrycjatyra@protonmail.com>
2020-04-11 14:45:53 +0100

    renamed venev name

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ['SECRET_KEY'] = 'c822vltgijo71ybzx1npe3'
-os.environ['MONGO_URI'] = 'mongodb+srv://patrycja:<@scor81C>@firstcluster-y49uk.mongodb.net/test?retryWrites=true&w=majority'
