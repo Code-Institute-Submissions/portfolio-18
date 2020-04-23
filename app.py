97e489e7660b1f23eb8903dca7dad02ea03e912c
Patrycja <patrycjatyra@protonmail.com>
2020-04-23 21:09:14 +0100

    added import flask-pymongo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for, request, abort
+from flask import Flask, render_template, redirect, url_for, request, abort
 import os
 from os import path
-
+from flask_pymongo import PyMongo
 
 
 
