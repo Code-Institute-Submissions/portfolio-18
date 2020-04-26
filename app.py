223daa2a20915693b8d43a9462e846c2394595db
Patrycja <patrycjatyra@protonmail.com>
2020-04-26 17:35:48 +0100

    removed mongo to login for testing

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ def skills():
 
 @app.route("/login", methods=['POST'])
 def login():
-    users = mongo.db.users
-    users.find_one({'name' : request.form['username']})
+    
     return render_template("pages/login.html", headTitle="Admin panel")
 
 @app.route("/contact", methods=['GET', 'POST'])
