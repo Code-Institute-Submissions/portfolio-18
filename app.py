bf96a8fccd5e8c5af6f90a339f78e70ddc502643
Patrycja <patrycjatyra@protonmail.com>
2020-04-28 18:35:07 +0100

    trying to fix bug to login

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def login():
     login_user = users.find_one({'username': request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
            session['username'] = request.form['username']
            return redirect( url_for('editor'))
     return 'Invalid username/password'
