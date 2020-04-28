3706d5c2fc44847a7d9fad671bf619723e325603
Patrycja <patrycjatyra@protonmail.com>
2020-04-28 17:25:21 +0100

    removed bug

--- app.py
+++ app.py
@@ -54,13 +54,14 @@ def login():
     For sensitive data that must be protected, such as passwords, bcrypt is an advisable choice.
     """
     users = mongo.db.users
-    login_user = users.find_one({'name': request.form['username']})
+    login_user = users.find_one({'username': request.form['username']})
 
     if login_user:
         if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
            session['username'] = request.form['username']
            return redirect( url_for('editor'))
     return 'Invalid username/password'
+
 
     
     login_password = users.find_one({'password': request.form['userpassword']})
