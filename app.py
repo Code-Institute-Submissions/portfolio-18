33f6d85f32183095886191081cc04fef65c94f38
Patrycja <patrycjatyra@protonmail.com>
2020-04-29 18:07:33 +0100

    removed text wrong request from login

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def login():
         if login_user:
             session['email'] = login_user['email']
             session['name'] = login_user['name']
-            return redirect(url_for('user'))
+            return redirect(url_for('admin'))
        
-        return 'Invalid username or password combination'
+      
 
 @app.route("/register", methods=['POST','GET'])
 def register():
