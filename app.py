45d5b82e5cc391c3e6bbabd2b0da0fe0282f3b71
Patrycja <patrycjatyra@protonmail.com>
2020-04-30 17:06:27 +0100

    changed form index to login as there was error

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def login():
 def register():
     email = session.get('email')
     if email:
-      return redirect(url_for('index'))
+      return redirect(url_for('login'))
     
     user = None
     if request.method == 'POST':
