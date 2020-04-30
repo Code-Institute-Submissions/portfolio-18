8b172e2302cb3cdd87f4cac70193e7be161b1a8b
Patrycja <patrycjatyra@protonmail.com>
2020-04-30 19:14:30 +0100

    changed mixed routing

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def login():
         if login_user:
             session['email'] = login_user['email']
             session['name'] = login_user['name']
-        return redirect(url_for('user'))
+        return redirect(url_for('admin'))
        
     return 'Invalid username or password combination'
        
