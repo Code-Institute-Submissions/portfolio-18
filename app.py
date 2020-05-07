ba9792b19e282958221df1c7e7f249230437f076
Patrycja <patrycjatyra@protonmail.com>
2020-05-07 21:40:50 +0100

    updated edit page if it interferes

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def edit_project(project_id):
     loggedIn = True if 'email' in session else False
 
     if not loggedIn:
-        return redirect(url_for('permission'))
+        return render_template('pages/permission.html', headTitle="Access denied")
     else:
         the_project = mongo.db.projects.find_one({"_id": ObjectId(project_id)})
         return render_template('pages/edit_project.html',  project=the_project, email=session['email'], headTitle="Edit")    
